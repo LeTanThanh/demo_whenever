d6ff82c7157bb290f3c6ad471cc732f2b9f47439
LeTanThanh <tanthanhcr95@gmail.com>
2019-06-25 12:38:45 +0700

    set_env

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+
 set :output, "log/cron.log"
-set :environment, :development
+set :environment, Rails.env
 
 every 1.minute do
   rake "user:random"
